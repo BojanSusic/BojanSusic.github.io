f738afe290bead6b1169a4bec176c111cb02a818
BojanSusic <60573368+BojanSusic@users.noreply.github.com>
2020-05-03 12:20:35 +0200

    nema poruke

---
+++ index.php
@@ -0,0 +1,18 @@
+<?PHP
+  
+if (isset($_POST['UserName'])) {
+    $UserName = $_POST['UserName'];
+} else {
+    $UserName = null;
+}
+  
+if (isset($_POST['Password'])) {
+    $Password = $_POST['Password'];
+} else {
+    $Password = null;
+}
+  
+  
+echo $UserName . " Is And Password Is " . $Password
+  
+?>
