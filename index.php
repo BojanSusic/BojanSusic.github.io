55697d33cbf607ed910f60e2cf38e0476cc93654
BojanSusic <60573368+BojanSusic@users.noreply.github.com>
2020-05-03 13:00:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?PHP
-  
-if (isset($_POST['UserName'])) {
-    $UserName = $_POST['UserName'];
-} else {
-    $UserName = null;
-}
-  
-if (isset($_POST['Password'])) {
-    $Password = $_POST['Password'];
-} else {
-    $Password = null;
-}
-  
-echo "TEST"
-echo $UserName . " Is And Password Is " . $Password
-  
+<form action="welcome.php" method="get">
+First name: <input type="text" name="fname">
+</form>
 ?>
